0f1ce93b2f1bd7ad35b7a87915ebcc3426faf172
Tulasi Paradarami <research800@gmail.com>
2016-02-27 19:35:23 -0800

    script to extract train and test datasets

--- scripts/r_related/train_test_split.R
+++ scripts/r_related/train_test_split.R
@@ -1,13 +1,13 @@
 set.seed(12345)
 
-train_test_split = function(pa_user) {
+train_test_split = function(restaurant) {
   
   # split user data into train and test
-  smp_size = floor(0.70 * nrow(pa_user))
-  train_ind = sample(seq_len(nrow(pa_user)), size = smp_size)
+  smp_size = floor(0.70 * nrow(restaurant))
+  train_ind = sample(seq_len(nrow(restaurant)), size = smp_size)
   
-  train_user = pa_user[train_ind, ]
-  test_user = pa_user[-train_ind, ]
+  train_restaurant = restaurant[train_ind, ]
+  test_restaurant = restaurant[-train_ind, ]
 
-  return (list(train_user, test_user))
+  return (list(train_restaurant, test_restaurant))
 }
